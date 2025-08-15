2966eef4609e07c2d80a65bdc35f44b94e9b0cdf
anwar108658 <anwar108658@gmail.com>
2025-08-15 18:20:26 +0500

    add Search

--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -5,6 +5,12 @@ const Search = () => {
   return (
     <div>
         <Header/>
+        <div className='max-w-[1200px] mx-auto flex'>
+            <div className='bg-gray-300 flex-4'>
+                <h1 className='text-2xl'>Properties for sale in UAE</h1>
+            </div>
+            <div className='bg-blue-100 flex-2'>hello2</div>
+        </div>
     </div>
   )
 }
