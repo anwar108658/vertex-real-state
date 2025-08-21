5274d553e75d2502ba44fd1208b24a73b8dba8e8
anwar108658 <anwar108658@gmail.com>
2025-08-21 17:52:13 +0500

    image preview

--- src/Pages/Property.jsx
+++ src/Pages/Property.jsx
@@ -8,10 +8,12 @@ import Button from '../components/Button';
 import Segment from '../components/Segment';
 import Avatar from 'antd/es/avatar/Avatar';
 import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { Image } from 'antd';
 
 const Property = () => {
     const location = useLocation();
     const { item } = location.state || {};
+    const [popupOpen, setPopupOpen] = React.useState(false);
     const popup = [{key: 'photos', value:`Photos (${item?.image?.length})`,icon:<FontAwesomeIcon icon={faImage} />},{key: 'map', value:'Map',icon:<FontAwesomeIcon icon={faLocationDot} />}];
     if (!item) {
         return <div className='h-[61vh] flex items-center justify-center text-gradient text-2xl font-semibold'>Property not found___<Link to="/search" className='font-normal text-blue-900 bg-blue-100 px-3 rounded-lg'>Go back</Link></div>;
@@ -21,12 +23,12 @@ const Property = () => {
     <>
         <div className='max-w-[1200px] mx-auto !overflow-hidden scrollbar-hide'>
             <div className='h-[80vh] flex gap-3'>
-                <div className='flex-5'><img className='w-full h-full object-cover hover:brightness-90 cursor-pointer rounded-lg' src={item?.image[0]} alt="im" /></div>
+                <div className='flex-5'><img className='w-full h-full object-cover hover:brightness-90 cursor-pointer rounded-lg' onClick={() => setPopupOpen(true)} src={item?.image[0]} alt="im" /></div>
                 <div className='flex-2 grid grid-rows-2 gap-3'>
-                    <div><img className='w-full h-full object-cover hover:brightness-90 cursor-pointer rounded-lg' src={item?.image[1]} alt="" /></div>
+                    <div><img className='w-full h-full object-cover hover:brightness-90 cursor-pointer rounded-lg' onClick={() => setPopupOpen(true)} src={item?.image[1]} alt="" /></div>
                     <div className='  grid  grid-rows-2 gap-3'>
-                        <img className='w-full h-full object-cover hover:brightness-90 cursor-pointer object-center rounded-lg' src={item?.image[2]} alt="" />
-                        <img className='w-full h-full object-cover hover:brightness-90 cursor-pointer object-center rounded-lg' src={item?.image[3]} alt="" />
+                        <img className='w-full h-full object-cover hover:brightness-90 cursor-pointer object-center rounded-lg' onClick={() => setPopupOpen(true)} src={item?.image[2]} alt="" />
+                        <img className='w-full h-full object-cover hover:brightness-90 cursor-pointer object-center rounded-lg' onClick={() => setPopupOpen(true)} src={item?.image[3]} alt="" />
                     </div>
                 </div>
             </div>
@@ -53,16 +55,29 @@ const Property = () => {
                 <div className='flex-2 min-w-[300px] bg-blue-100'></div>
             </div>
         </div>
-        <div className='w-full h-screen bg-black/60 backdrop-blur-sm fixed top-0 left-0 z-50 flex items-center justify-center'>
-            <div className='relative w-full max-w-4/5 h-[90vh] flex flex-col gap-3 p-5 px-7 bg-white rounded-lg'>
-                <button className='absolute text-[1rem] flex items-center top-2 right-2 cursor-pointer'>
+
+        <div onClick={() => setPopupOpen(false)} className={`${popupOpen ? "flex":"hidden"} w-full h-screen bg-black/60 backdrop-blur-sm fixed top-0 left-0 z-50  items-center justify-center ${popupOpen ? 'block' : 'hidden'}`}>
+            <div onClick={(e) => e.stopPropagation()} className='relative w-full max-w-4/6 h-[85vh] flex flex-col gap-3 p-5 px-7 bg-white rounded-lg'>
+                <button onClick={() => setPopupOpen(false)} className='absolute text-[1rem] flex items-center top-2 right-2 cursor-pointer'>
                     <FontAwesomeIcon size='md' color='red' icon={faXmark}/>
                 </button>
-                <Segment size='sm' className='w-full' options={popup}/>
+                <Segment size='md' className='w-full' options={popup}/>
                 <div className='flex-1 grid grid-cols-2 gap-3 overflow-auto'>
-                    {item?.image.length > 0 && item.image.map((img, index) => (
-                        <img key={index} className='w-full h-[400px] cursor-pointer rounded-lg' src={img} alt={`Image ${index}`} />
-                    ))}
+                <Image.PreviewGroup
+                items={item.image} // 👈 array of images
+                width={1000}
+                preview={{width: '800', height: '800px',minScale: 0.1,maxScale: 2,bodyStyle: {backgroundColor: 'rgba(0, 0, 0, 0.5)'},className:"scroll-y-hidden",wrapStyle: {overflow: 'hidden'},maskAnimation: {show: true}}}
+                >
+                {item.image.map((img, index) => (
+                    <Image
+                    key={index}
+                    src={img}
+                    alt={`Image ${index}`}
+                    className="w-full !h-[60vh] cursor-pointer rounded-lg"
+                    />
+                ))}
+                </Image.PreviewGroup>
+
                 </div>
                 <div className='flex justify-between items-center'>
                     <div className='flex items-center gap-3'>
@@ -77,6 +92,7 @@ const Property = () => {
                 </div>
             </div>
         </div>
+
     </>
   )
 }
